7f83593070ae593b59ebe5645b1f1038cdcbd78d
Nick Jaeger <jaegernh@uwec.edu>
2017-02-06 06:58:53 -0600

    added prompt function with validation

--- gui.js
+++ gui.js
@@ -1,7 +1,31 @@
 /*
-  You do not need to modify this file.
+  You do not need to modify this file. However, you may.
 */
 
 function app(people){
 
+}
+
+function searchByName(people){
+
+}
+
+function searchByTraits(people){
+
+}
+
+function mainMenu(person, people){
+
+}
+
+
+function promptFor(question, valid){
+  do{
+    var response = prompt(question);
+  } while(!response || !valid(response));
+  return response;
+}
+
+function yesNo(input){
+  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
 }
