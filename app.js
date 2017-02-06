452ce222604b757350350bce0c75654031700811
Nick Jaeger <jaegernh@uwec.edu>
2017-02-06 09:30:54 -0600

    added stubs to app

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 /*
   Build all of your functions for the application below.
+	Some functions have been stubbed out.
 */
+
+function filterByName(firstName, lastName, people){
+
+	// return an aray containing the people matching the name
+}
+
+function getFamily(person, people){
+
+	// return an array containing the family members (objects)
+}
+
+function getDescendants(person, people){
+
+	// return an array containing the descendants (objects)
+}
