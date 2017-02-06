e38bcdd24c35dca7818661f548acd8cb9d5362d1
Nick Jaeger <jaegernh@uwec.edu>
2017-02-06 06:32:48 -0600

    changed application call

--- gui.js
+++ gui.js
@@ -1,30 +1,3 @@
-function initSearch(people){
-	;
-}
-function initSearchByName(){
-	;
-}
-function initSearchByTraits(){
-	;
-}
-function displayResults(){
-	;
-}
-function isNumeric() {
-	;
-}
-function getAge(){
-	;
-}
-function getHeight(){
-	;
-}
-function getWeight(){
-	;
-}
-function getEye(){
-	;
-}
-function getOccupation(){
-	;
+function app(people){
+
 }
